a356f2abf091f15d76bfa964795ce6bec558a58e
Luphia Chang <luphia@outlook.com>
2014-08-28 18:28:07 +0800

    Use native google+ authentication

--- public/js/app.js
+++ public/js/app.js
@@ -33,3 +33,9 @@ Kamato.directive('message', function($compile) {
 		templateUrl: 'widgets/chat/template-message.html'
 	}
 });
+
+(function() {
+       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+       po.src = 'https://apis.google.com/js/client:plusone.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+     })();
