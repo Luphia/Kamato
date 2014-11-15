321ade7056fffde944512b93fef379d203eb3c70
Luphia Chang <luphia@outlook.com>
2014-11-15 22:32:16 +0800

    Update EasyDB Module

--- services/DBConnector/MongoDB.js
+++ services/DBConnector/MongoDB.js
@@ -1,8 +1,43 @@
 module.exports = function() {
+	var connect = function() {},
+		disconnect = function() {},
+		tableExist = function() {},
+		getSchema = function() {},
+		setSchema = function() {},
+		getID = function() {},
+		checkID = function() {},
+		sql = function() {},
+		listTable = function() {},
+		getTable = function() {},
+		postTable = function() {},
+		putTable = function() {},
+		deleteTable = function() {},
+		listData = function() {},
+		getData = function() {},
+		postData = function() {},
+		putData = function() {},
+		deleteData = function() {};
 
 	var MongoDB = {
-
-	}
+		connect: connect,
+		disconnect: disconnect,
+		tableExist: tableExist,
+		getSchema: getSchema,
+		setSchema: setSchema,
+		getID: getID,
+		checkID: checkID,
+		sql: sql,
+		listTable: listTable,
+		getTable: getTable,
+		postTable: postTable,
+		putTable: putTable,
+		deleteTable: deleteTable,
+		listData: listData,
+		getData: getData,
+		postData: postData,
+		putData: putData,
+		deleteData: deleteData
+	};
 
 	return MongoDB;
 };
