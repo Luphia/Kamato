f927b9003c91b705a9112ad76a2a49683b1552f1
MR.LIN <t54185@yahoo.com.tw>
2014-12-04 18:45:41 +0800

    Add Login fnc

--- services/Controllers/user.js
+++ services/Controllers/user.js
@@ -12,63 +12,65 @@
  */
 
 var Result = require('../Classes/Result.js')
-,	UserManager = require('../Classes/UserManager.js')
-,	EasyDB = require('../Classes/EasyDB.js');
+, UserManager = require('../Classes/UserManager.js')
+, EasyDB = require('../Classes/EasyDB.js');
 
 var config
-,	logger
-,	easyDB
-,	UserManager;
+, logger
+, easyDB
+, userManager;
 
 module.exports = {
-	init: function(_config, _logger, _route) {
-		config = _config;
-		logger = _logger;
-		easyDB = new EasyDB(config);
-		easyDB.connect(config.option);
-		userManager = new UserManager(easyDB);
+    init: function (_config, _logger, _route) {
+        config = _config;
+        logger = _logger;
+        easyDB = new EasyDB(config);
+        easyDB.connect(config.option);
+        userManager = new UserManager(easyDB);
 
-		_route.get('/me', module.exports.data);
-		_route.post('/login', module.exports.login);
-		_route.get('/oauth2/:platform', module.exports.outerLogin);
-		_route.get('/oauth2/:platform/*', module.exports.outerLogin);
-	},
-	data: function(req, res, next) {
-	    var userData = req.session || {};
-	    userData.text = 'test! session msg and print session';
-		userData.ip = req.connection.remoteAddress;
-		res.send(userData);
-	},
-	login: function(req, res, next) {
-		res.result = new Result();
-		var auth = {
-			account: req.body.account,
-			password: req.body.password
-		};
-		delete auth.password; //-- I don't know the hash rule of password
+        _route.get('/me', module.exports.data);
 
-		var userData = easyDB.dbListData(config.userTable, auth);
-		if(userData.length > 0) {
-			var msg = "login success";
-			res.result.response(next, 1, msg, userData[0]);
-		}
-		else {
-			var msg = "login fail";
-			res.result.response(next, 0, msg);
-		}
-	},
-	outerLogin: function(req, res, next) {
-		res.result = new Result();
-		var params = {};
+        _route.get('/login', module.exports.login);
+        _route.get('/loginout', module.exports.login);
+        _route.post('/addtoken', module.exports.login);
+        _route.post('/register', module.exports.login);
+        _route.get('/check', module.exports.login);
 
-		for(var key in _req.query) {
-			params[key] = _req.query[key];
-		}
-		for(var key in _req.params) {
-			params[key] = _req.params[key];
-		}
-		
-		res.result.response(next, 1, 'Login with: ' + _req.params.platform, data);
-		//userManager.findByPlatform()
-	}
+        _route.get('/oauth2/:platform', module.exports.outerLogin);
+        _route.get('/oauth2/:platform/*', module.exports.outerLogin);
+    },
+    data: function (req, res, next) {
+        var userData = req.session || {};
+        userData.text = 'test! session msg and print session';
+        userData.ip = req.connection.remoteAddress;
+        res.send(userData);
+    },
+    login: function (req, res, next) {
+        res.result = new Result();
+        var data = req.body;
+        var x = userManager.login(data);
+        if (x == false) {
+            res.result.response(next, 0, 'Login Fail');
+            res.session = userManager.login(data);
+            next();
+        } else {
+            res.result.response(next, 1, 'Login Success', x);
+            res.session = x;
+            next();
+        };
+    },
+    outerLogin: function (req, res, next) {
+        res.result = new Result();
+        var params = {};
+
+        for (var key in _req.query) {
+            params[key] = _req.query[key];
+        }
+        for (var key in _req.params) {
+            params[key] = _req.params[key];
+        }
+
+        res.result.response(next, 1, 'Login with: ' + _req.params.platform, data);
+        //userManager.findByPlatform()
+    }
 };
