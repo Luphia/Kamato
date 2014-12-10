46692ae99fc4eefc5eeedd204bbee43c99b529a8
MR.LIN <t54185@yahoo.com.tw>
2014-12-10 14:04:48 +0800

    Add member fnc

--- services/Controllers/user.js
+++ services/Controllers/user.js
@@ -30,6 +30,7 @@ module.exports = {
 
         _route.get('/me', module.exports.data);
 
+        //master
         _route.post('/login', module.exports.login);
         _route.get('/loginout', module.exports.loginout);
         _route.get('/addtoken', module.exports.addtoken);
@@ -37,6 +38,15 @@ module.exports = {
         _route.get('/check', module.exports.check);
         _route.post('/forgot', module.exports.forgot);
         _route.post('/repassword', module.exports.repassword);
+
+        //user
+        _route.post('/ulogin', module.exports.ulogin);
+        _route.get('/uloginout', module.exports.uloginout);
+        _route.get('/uaddtoken', module.exports.uaddtoken);
+        _route.post('/uregist', module.exports.uregist);
+        _route.get('/ucheck', module.exports.ucheck);
+        _route.post('/uforgot', module.exports.uforgot);
+        _route.post('/urepassword', module.exports.urepassword);
 
         _route.get('/oauth2/:platform', module.exports.outerLogin);
         _route.get('/oauth2/:platform/*', module.exports.outerLogin);
@@ -46,15 +56,11 @@ module.exports = {
         userData.ip = req.connection.remoteAddress;
         res.send(userData);
     },
+    //master
     login: function (req, res, next) {
         res.result = new Result();
         var data = req.body;
-        var x = false;
-        if (data.user == 1) {
-            x = userManager.ulogin(data);
-        } else {
-            x = userManager.login(data);
-        };
+        var x = userManager.login(data);
         var s = req.session;
         if (x == false) {
             res.result.response(next, 0, 'Login Fail');
@@ -120,6 +126,78 @@ module.exports = {
             res.result.response(next, 1, 'Repassword Success', x);
         };
     },
+    //user
+    ulogin: function (req, res, next) {
+        res.result = new Result();
+        var data = req.body;
+        var x = userManager.ulogin(data);
+        var s = req.session;
+        if (x == false) {
+            res.result.response(next, 0, 'Login Fail');
+        } else {
+            s._id = x._id;
+            s.name = x.name;
+            s.picture = x.picture;
+            s.login = 1;
+            res.result.response(next, 1, 'Login Success', x);
+        };
+    },
+    uloginout: function (req, res, next) {
+        res.result = new Result();
+        req.session.destroy();
+        res.result.response(next, 1, 'Session Destroy');
+    },
+    uaddtoken: function (req, res, next) {
+        res.result = new Result();
+        var data = req.body;
+        var x = userManager.addToken(data);
+        if (x == false) {
+            res.result.response(next, 0, 'Addtoken Fail');
+        } else {
+            res.result.response(next, 1, 'Addtoken Success', x);
+        };
+    },
+    uregist: function (req, res, next) {
+        res.result = new Result();
+        var data = req.body;
+        var x = userManager.uadd(data);
+        if (x == false) {
+            res.result.response(next, 0, 'Regist Fail');
+        } else {
+            res.result.response(next, 1, 'Regist Success', x);
+        };
+    },
+    ucheck: function (req, res, next) {
+        res.result = new Result();
+        var s = req.session;
+        if (s && s.name != null && s.login == 1) {
+            res.result.response(next, 1, 'Check Success', s);
+        } else {
+            res.result.response(next, -2, 'Check Fail');
+        };
+    },
+    uforgot: function (req, res, next) {
+        res.result = new Result();
+        var data = req.body;
+        var x = userManager.uforgot(data);
+        if (x == false) {
+            res.result.response(next, 0, 'Forgot Fail');
+        } else {
+            res.result.response(next, 1, 'Forgot Success', x);
+        };
+    },
+    urepassword: function (req, res, next) {
+        res.result = new Result();
+        var data = req.body;
+        var x = userManager.urepassword(data);
+        if (x == false) {
+            res.result.response(next, 0, 'Repassword Fail');
+        } else {
+            res.result.response(next, 1, 'Repassword Success', x);
+        };
+    },
+
+
 
     outerLogin: function (req, res, next) {
         res.result = new Result();
