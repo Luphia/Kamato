36b971abe98af73ea9155f3140849216228bad98
Ceall Shih <ceall8650@gmail.com>
2014-12-17 10:14:41 +0800

    [ API ] 新增API
    [新增] REST Methods, SQL Methods, Public API, Category Tag
    [修改] Schema 名稱( 移除 filed, 新增Visible)

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -353,32 +353,49 @@ KamatoControllers.controller('PlatformCtrl', function ($scope, $http, $modal, ng
 
 	}
 
-	$scope.api_list = true;
-	$scope.create_api = false;
-	$scope.req_chart = false;
+	$scope.api_list = true;		   //API管理頁面
+	$scope.create_api = false;     //隱藏新增API頁面
+	$scope.req_chart = false;	   //隱藏API活躍頁面
+	$scope.ca_tag = ['sport','running','Nike','running','Runkeeper','sn','google+','sn','fb'];
 
-	$scope.create_api_page = function(){
+	$scope.create_api_page = function(){   
+	//表單初始化
 		$scope.api_list = false;
 		$scope.create_api = true;
+		// 表單require
 		$scope.new_api_required = true;
+		$scope.select_rest_mt = true;
+		// 表格初始化
+		$scope.new_api_name = "";
+		$scope.new_tag = "";
+		for (var r in $scope.rest_methods){
+			$scope.rest_methods[r]['sql'] = "";
+			$scope.rest_methods[r]['checked'] = false;
+			$scope.rest_methods[r]['required'] = true;
+		}
 	}
 
+	// $scope.init = function(){
+
+	// }
+
+	// $scope.destory = function(){
+
+	// }
+
 	$scope.return_api_page = function(){
-		$scope.new_api_name = "";
-		$scope.new_api_required = false;
 		$scope.api_list = true;
 		$scope.create_api = false;
 		$scope.req_chart = false;
-		$scope.rest_methods = [
-			{'name': 'GET', 'checked': false},
-			{'name': 'POST', 'checked': false},
-			{'name': 'PUT', 'checked': false},
-			{'name': 'DELETE', 'checked': false}
-		]
-		$scope.GET = false;
-		$scope.POST = false;
-		$scope.PUT = false;
-		$scope.DELETE = false;
+
+		//取消require
+		$scope.select_rest_mt = false;
+		$scope.new_api_required = false;
+		// 清空sql指令
+		$scope.command_sample = "";
+		for (var r in $scope.rest_methods){
+			$scope.rest_methods[r]['checked'] = false;
+		}
 	}
 
 	$scope.show_apiReq_chart = function(){
@@ -391,17 +408,24 @@ KamatoControllers.controller('PlatformCtrl', function ($scope, $http, $modal, ng
 	}
 
 	$scope.api_table_head = [
-		{'name': 'API List'},
+		{'name': 'API Name'},
 		{'name': 'Category'},
-		{'name': 'Field'},
-		{'name': ''}
+		{'name': 'Visible'}, 
+		{'name': ''}//delete btn
+	]  
+
+	$scope.api_cate = [
+		{'name': 'Sport', 'required': true},
+		{'name': 'Social Network', 'required': true},
+		{'name': 'Weather', 'required': true},
+		{'name': 'Goverment', 'required': true},
 	]
 
 	$scope.apiList = [
-		{'name': 'NikePlus', 'cate': [{'property': 'Nutrition'}], 'field': 'Sport'},
-		{'name': 'Runkeeper', 'cate':  [{'property': 'Nutrition'}, {'property': 'Friend'}], 'field': 'Sport'},
-		{'name': 'GooglePlus', 'cate': [{'property': 'Activity'}, {'property': 'Profile'}], 'field': 'Social Network'},
-		{'name': 'Facebook', 'cate': [{'property': 'Profile'}, {'property': 'Activity'}, {'property': 'Friend'}], 'field': 'Social Network'},
+		{'name': 'NikePlus', 'cate': ['sport','running','Nike'], 'visible': 'Public'},
+		{'name': 'Runkeeper', 'cate': ['sport','running','Runkeeper'], 'visible': 'Public'},
+		{'name': 'GooglePlus', 'cate': ['sn','google+'], 'visible': 'Public'},
+		{'name': 'Facebook', 'cate': ['sn','fb'], 'visible': 'Public'},
 	]
 
 	$scope.apiSearch = [];
@@ -411,55 +435,59 @@ KamatoControllers.controller('PlatformCtrl', function ($scope, $http, $modal, ng
 	}
 
 	$scope.rest_methods = [
-		{'name': 'GET', 'checked': false},
-		{'name': 'POST', 'checked': false},
-		{'name': 'PUT', 'checked': false},
-		{'name': 'DELETE', 'checked': false}
+		{'name': 'GET', 'checked': false, 'sql': '', 'required': true},
+		{'name': 'POST', 'checked': false, 'sql': '', 'required': true},
+		{'name': 'PUT', 'checked': false, 'sql': '', 'required': true},
+		{'name': 'DELETE', 'checked': false, 'sql': '', 'required': true},
 	]
 
-	$scope.click_rest_mt = function(methods, r_m){
-
-		switch(methods.name){
-			case "GET":
-				$scope.GET = !$scope.GET;
-				break;
-			case "POST":
-				$scope.POST = !$scope.POST;
-				break;
-			case "PUT":
-				$scope.PUT = !$scope.PUT;
-				break;
-			case "DELETE":
-				$scope.DELETE = !$scope.DELETE;
-				break;
-		} 
-	}
-
-
-	$scope.rest_command=[
-		{'name': 'Get SQL','method': 'GET'},
-		{'name': 'Post SQL','method': 'POST'},
-		{'name': 'PUT SQL','method': 'PUT'},
-		{'name': 'DELETE SQL','method': 'DELETE'},
-	]
-
-	$scope.is_public = "Public";
-	$scope.api_public = function(name){
-		$scope.is_public = name;
-	}
-
-	$scope.api_submit = function() {
-		// 確認REST方法是否勾選
-		console.log($scope.select_rest_mt);
+	$scope.click_checkbox = function(ch_option){
+		// 確認是否有勾選REST方法
 		var is_click = false;
-		for (var r in $scope.rest_methods){
-			if ($scope.rest_methods[r].checked == true){
+		for (var c in ch_option){
+			//有勾選
+			if (ch_option[c].checked == true){
 				is_click = true;
-				$scope.select_rest_mt = false;
 			}
 		}
+		//沒勾選
 		if(is_click == false){
-			$scope.select_rest_mt = true;
+			//加入勾選提示
+			for (var c in ch_option){
+				ch_option[c].required = true;;
+			}
+		}
+		//有勾選
+		else{
+			//取消勾選提示, 有勾選其中一個選項即可
+			for (var c in ch_option){
+				ch_option[c].required = false;
+			}
+		}
+	}
+
+	$scope.visible_clicked = "Public";
+	$scope.api_public = function(name){
+		$scope.visible_clicked = name;
+	}
+
+	$scope.new_tag="";
+
+	// $scope.ca_tag_keyup = function(e){
+	// 	//press enter
+	// 	if(e.keyCode == 13){
+	// 		var tag = $scope.new_tag;
+
+	// 	}
+	// }
+
+	$scope.typing_tag = false;
+	$scope.api_submit = function() {
+	// 送出表單
+		if( $scope.typing_tag == false ){
+			$scope.apiList.push({'name': this.new_api_name, 'cate': [$scope.new_tag], 'visible': $scope.visible_clicked});
+			$scope.create_api = false;
+			$scope.api_list = true;
 		}
 	}
 // ====================== api bottom======================
