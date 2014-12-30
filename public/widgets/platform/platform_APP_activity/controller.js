c9030ec4e5176f2c5fc92ddaab933241a5415cf3
MR.LIN <t54185@yahoo.com.tw>
2014-12-30 11:58:41 +0800

    Fix $routeParams for controller

--- public/widgets/platform/platform_APP_activity/controller.js
+++ public/widgets/platform/platform_APP_activity/controller.js
@@ -27,7 +27,7 @@ Kamato.register.controller('appActivityCtrl', function ($scope, $http, $modal, n
     $scope.app_oper = [
 		// {'tip': 'Home', 'icon': 'sa-list-home'},
 		{ 'tip': 'Back', 'icon': 'sa-list-back', 'link': '#/platform/APP' },
-		{ 'tip': 'Activity', 'icon': 'sa-list-activity', 'link': '#/platform/APP_activity' },
+		{ 'tip': 'Activity', 'icon': 'sa-list-activity', 'link': '#/platform/' + $routeParams.APP + '/APP_activity' },
 		{ 'tip': 'Info.', 'icon': 'sa-list-info', 'link': '#/platform/APP_info' },		// {'tip': 'Upload', 'icon': 'sa-list-upload', 'link': '#/platform/APP/upload'}
  		{ 'tip': 'Folder', 'icon': 'sa-list-folder', 'link': '#/platform/APP_folder' },
     ];
@@ -58,6 +58,7 @@ Kamato.register.controller('appActivityCtrl', function ($scope, $http, $modal, n
         if (t1) {
             $timeout.cancel(t1);
         };
+        socket.disconnect();
     });
 
     $scope.init = function () {
--- public/widgets/platform/platform_APP_folder/controller.js
+++ public/widgets/platform/platform_APP_folder/controller.js
@@ -1,32 +1,32 @@
-Kamato.register.controller('appFolderCtrl', function ($scope, $http, $modal, ngDialog, $rootScope){
-// ====================== APP管理 top======================
-	$scope.app_oper = [
+Kamato.register.controller('appFolderCtrl', function ($scope, $http, $modal, ngDialog, $rootScope, $routeParams) {
+    // ====================== APP管理 top======================
+    $scope.app_oper = [
 		// {'tip': 'Home', 'icon': 'sa-list-home'},
-		{'tip': 'Back', 'icon': 'sa-list-back', 'link': '#/platform/APP'},
-		{'tip': 'Activity', 'icon': 'sa-list-activity', 'link': '#/platform/APP_activity'},
-		{'tip': 'Info.', 'icon': 'sa-list-info','link': '#/platform/APP_info'},
-		{'tip': 'Folder', 'icon': 'sa-list-folder', 'link': '#/platform/APP_folder'},
+		{ 'tip': 'Back', 'icon': 'sa-list-back', 'link': '#/platform/APP' },
+		{ 'tip': 'Activity', 'icon': 'sa-list-activity', 'link': '#/platform/APP/' + $routeParams.APP + '/APP_activity' },
+		{ 'tip': 'Info.', 'icon': 'sa-list-info', 'link': '#/platform/APP_info' },
+		{ 'tip': 'Folder', 'icon': 'sa-list-folder', 'link': '#/platform/APP_folder' },
 		// {'tip': 'Upload', 'icon': 'sa-list-upload', 'link': '#/platform/APP/upload'}
-	];
+    ];
 
 
-	$scope.fileNameChange = function(elem){
-		$scope.files = elem.files;	
-		$scope.$apply();
-	}
+    $scope.fileNameChange = function (elem) {
+        $scope.files = elem.files;
+        $scope.$apply();
+    }
 
-	$scope.test_file= {
-			'Path':'public/',
-			'files':[
-				{'datatype': 'folder', 'name': 'css', 'icon': 'sa-list-folder','file_size': '', 'Last_revise': '2014/11/25'},
-				{'datatype': 'folder', 'name': 'js', 'icon': 'sa-list-folder','file_size': '', 'Last_revise': '2014/11/25'},
-				{'datatype': 'folder', 'name': 'lib', 'icon': 'sa-list-folder','file_size': '', 'Last_revise': '2014/11/25'},
-				{'datatype': 'folder', 'name': 'simple', 'icon': 'sa-list-folder','file_size': '', 'Last_revise': '2014/11/25'},
-				{'datatype': 'folder', 'name': 'widget', 'icon': 'sa-list-folder','file_size': '', 'Last_revise': '2014/11/25'},
-				{'datatype': 'html', 'name': 'index.html', 'icon': 'sa-list-file','file_size': '234kb', 'Last_revise': '2014/11/25'},
-				{'datatype': 'txt', 'name': 'README.md', 'icon': 'sa-list-file','file_size': '234kb', 'Last_revise': '2014/11/25'},
-			],
-		}
-// ====================== APP管理 bottom======================
+    $scope.test_file = {
+        'Path': 'public/',
+        'files': [
+            { 'datatype': 'folder', 'name': 'css', 'icon': 'sa-list-folder', 'file_size': '', 'Last_revise': '2014/11/25' },
+            { 'datatype': 'folder', 'name': 'js', 'icon': 'sa-list-folder', 'file_size': '', 'Last_revise': '2014/11/25' },
+            { 'datatype': 'folder', 'name': 'lib', 'icon': 'sa-list-folder', 'file_size': '', 'Last_revise': '2014/11/25' },
+            { 'datatype': 'folder', 'name': 'simple', 'icon': 'sa-list-folder', 'file_size': '', 'Last_revise': '2014/11/25' },
+            { 'datatype': 'folder', 'name': 'widget', 'icon': 'sa-list-folder', 'file_size': '', 'Last_revise': '2014/11/25' },
+            { 'datatype': 'html', 'name': 'index.html', 'icon': 'sa-list-file', 'file_size': '234kb', 'Last_revise': '2014/11/25' },
+            { 'datatype': 'txt', 'name': 'README.md', 'icon': 'sa-list-file', 'file_size': '234kb', 'Last_revise': '2014/11/25' },
+        ],
+    }
+    // ====================== APP管理 bottom======================
 
 });
