747ec97681afa0e2267c56ba47b0488826ab240d
MR.LIN <t54185@yahoo.com.tw>
2014-12-31 12:10:05 +0800

    ttest

--- services/SocketServer.js
+++ services/SocketServer.js
@@ -252,9 +252,11 @@ function registerNamespace(name) {
 
     nsp[name].on('connection', function (socket) {
         console.log("I am in namespace: " + nsp[name].name);
-        nsps(socket, nsp[name].name)
+        logger.info.info("I am in namespace: " + nsp[name].name);
+      //  nsps(socket, nsp[name].name)
         socket.on('disconnect', function () {
             console.log("I was in namespace: " + nsp[name].name);
+            logger.info.info("I was in namespace: " + nsp[name].name);
         });
     });
 
